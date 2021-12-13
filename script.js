abbadc0d382639795a7ce14a647d9adee8940a51
bweinmann <bsweinmann@gmail.com>
2021-12-13 12:45:26 -0500

    API call successful, object rendered in browser console

--- script.js
+++ script.js
@@ -4,12 +4,7 @@
 async function fetchCivInfo() {
   try {
     const url = "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations";
-    const res = await axios.get(url, {
-      headers: {
-        'Content-Type': 'application/json',
-          "Access-Control-Allow-Origin": "*",
-      }
-    });
+    const res = await axios.get(`https://boiling-mountain-84087.herokuapp.com/${url}`)
     console.log(res);
   } catch(error) {
     console.error(error)};
